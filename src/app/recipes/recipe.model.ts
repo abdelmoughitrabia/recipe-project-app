c760f0974319b0ebd6a6cb0fa0534f18e828bef5
Abdelmoughit Rabia <abdelmoughit.rabia@neoxia.com>
2018-12-28 16:15:48 +0100

    3 commit

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,11 @@
 export class Recipe {
   public name: string;
-  public descriptio: string;
+  public description: string;
   public path: string;
 
-  constructor(name: string, descriptio: string, path: string) {
+  constructor(name: string, description: string, path: string) {
     this.name = name;
-    this.descriptio = descriptio;
+    this.description = description;
     this.path = path;
   }
 }
