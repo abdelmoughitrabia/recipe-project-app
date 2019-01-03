3b369b45765e18f616bcc9cb861f60637693a96a
Abdelmoughit Rabia <abdelmoughit.rabia@neoxia.com>
2019-01-03 18:50:03 +0100

    5 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recipe-project-app';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipes';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+
 }
